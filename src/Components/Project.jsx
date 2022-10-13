081c526c9cbfdc34a422c36330ac314a277909fb
Kanyonga Chouella <kanyonga.chouell@gmail.com>
2022-10-13 20:25:22 +0300

    Projects_cards

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -5,6 +5,7 @@ import Chousnellor from '../img/Chousnellor.png'
 import Gechou from '../img/Gechou.png'
 import weshare from '../img/weshare.png'
 import { Swiper,SwiperSlide } from 'swiper/react'
+import GoTo from '../img/GoTo.png'
 import 'swiper/css';
 const Project = () => {
   return (
@@ -22,50 +23,63 @@ className={'Projects-slider'}
 >
   <SwiperSlide className='card'>
 <img src={Chousnellor} alt=""/>
-<span>chousnellor</span>
+<div className="card_desc">
+<span>Chousnellor</span>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-optio, eaque rerum! Provident similique accusantium nemo autem</p>
-<a  href='http://chousnellor.herokuapp.com/'>
-  <button  className="button P-button">Go to</button>
+molestiae quas vel sint commodi repudiandae consequuntu</p>
+<a  href='http://chousnellor.herokuapp.com/' style={{textDecoration: 'none'}}>
+  <button  className="button p-button">
+     <img src={GoTo}/>Go To</button>
     </a>
-
+</div>
   </SwiperSlide>
    <SwiperSlide className='card'>
 <img src={Gechou} alt="" />
+<div className="card_desc">
 <span>Gechou</span>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
 molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-optio, eaque rerum! Provident similique accusantium nemo autem</p>
-<a  href=''>
-  <button  className="button P-button">Go to</button>
+</p>
+<a  href='' style={{textDecoration: 'none'}}>
+  <button  className="button p-button" >
+      <img src={GoTo}/>Go To</button>
     </a>
+    </div>
   </SwiperSlide>
+
+
    <SwiperSlide className="card" >
 <img src={weshare} alt=""/>
-
-  <span>WeShare</span>
+<div className="card_desc">
+<span>WeShare</span>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
 molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-optio, eaque rerum! Provident similique accusantium nemo autem</p>
- <a href='https://quiet-shape-6288.on.fleek.co/'>
-  <button  className="button P-button">Go to</button>
+</p>
+
+  <a href='https://quiet-shape-6288.on.fleek.co/' style={{textDecoration: 'none'}}>
+  <button  className="button p-button">
+    <img src={GoTo}/>
+    Go To</button>
 </a>
 
+ 
+</div>
   </SwiperSlide>
+
+
   <SwiperSlide className='card'>
 <img src={Gechou} alt=""/>
+<div className="card_desc">
 <span>Gechou</span>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-optio, eaque rerum! Provident similique accusantium nemo autem</p>
-<a  href=''>
-  <button className="button P-button">Go to</button>
+molestiae quas vel sint commodi consequuntur</p>
+<a  href='' style={{textDecoration: 'none'}}>
+  <button className="button p-button">
+    <img src={GoTo}/>
+    Go To</button>
     </a>
+</div>
+
   </SwiperSlide>
 </Swiper>
 </div>
@@ -94,12 +108,34 @@ const Container=styled.div`
 }
 .card {
   width:250px;
-   background: rgba(255, 255, 255, 0.36);
-    
+   background: rgba(255, 255, 255, 0.66);
     border-radius: 20px;
-    padding: 0px 15px 1rem 10px;
+    padding: 0px 0px 1rem 0px;
 gap:10px;
+
+p{
+  color: gray;
+    font-size: 14px;
 }
+}
+.card:hover{
+  transform:scale(1.15);
+  transition:300ms ease-in;
+
+}
+
+.card span:nth-of-type(1){
+color: black;
+  font-size:30px;
+  font-weight:bold;
+}
+.card_desc{
+margin:20px;
+img{
+  width:10px;
+}
+}
+
 
 .Projects .swiper{
   overflow :visible !important;
@@ -115,8 +151,39 @@ gap:10px;
 
 .Projects img{
   width:20rem;
-    filter: drop-shadow(-12px 15px 13px rgba(0, 0, 0, 0.25));
       border-radius: 10px;
+}
+.p-button {
+  display:flex;
+  flex-direction:row;
+  justify-content: center;
+  align-item:center;
+    box-shadow: 0px 19px 60px rgba(0, 0, 0, 0.08);
+    border-radius: 7px;
+    border: none;
+    padding-left:17px;
+   padding-right:17px;
+   padding-top:10px;
+   padding-bottom:10px;
+    font-size: 15px;
+position:relative;
+left:270px;
+gap:10px;
+
+
+img{
+  width:20px;
+}
+
+}
+ 
+
+
+.p-button:hover {
+    transform: scale(1.1);
+    cursor: pointer;
+     border: 1px solid #dc4848;
+
 }
 `
 export default Project
